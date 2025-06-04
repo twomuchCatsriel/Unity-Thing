46d5ee989ca5523d2fd7cac9f8a43610a3d55283
catsriel <danbro07@imkatta.no>
2025-06-04 11:16:41 +0200

    Start work on a bullet

--- the thing/Assets/Scripts/basicEnemy.cs
+++ the thing/Assets/Scripts/basicEnemy.cs
@@ -4,11 +4,13 @@ public class basicEnemy : MonoBehaviour
 {
     public GameObject plr;
     GameObject DetectionRange;
+    GameObject bulletRaw;
     Rigidbody2D rb2d;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         DetectionRange = gameObject.transform.Find("Enemy_Detection_Range").gameObject;
+        bulletRaw = gameObject.transform.Find("bullet").gameObject;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
@@ -34,6 +36,18 @@ public class basicEnemy : MonoBehaviour
             {
                 Debug.Log("Player is on top.");
             }
+        }
+    }
+
+    void createBullet(int dir)
+    {
+        GameObject CloneBullet = Instantiate(bulletRaw);
+        Rigidbody2D rb = CloneBullet.GetComponent<Rigidbody2D>();
+        // 1 or 0 depending on the direction of the bullet
+
+        if (dir == 0)
+        {
+            
         }
     }
 }
